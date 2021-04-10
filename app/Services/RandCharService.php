f3c93f7febdd1111ff3d7937f25be5e984c9232c
wukun <wukun@360.cn>
2021-04-10 00:26:44 -0700

    1

--- app/Services/RandCharService.php
+++ app/Services/RandCharService.php
@@ -24,11 +24,28 @@ class RandCharService{
         if($data['len'] > 1024){
             throw new ServiceException(MessageCode::INVALID_LEN_OUT);
         }
-        $char = self::parseChar(explode('|', $data['char']));
+        $charItems = explode('|', $data['char']);
 
-        return Util::makeCode($char, $data['len']);
+        $mainChar = self::makeMainChar($charItems);
+        $char = self::parseChar($charItems);
+        $rest = Util::makeCode($char, $data['len'] - count($charItems));
+        $arr = str_split($mainChar . $rest);
+        shuffle($arr);
+        return implode($arr,'');
     }
 
+    public static function makeMainChar($charArray = []){
+        $char = "";
+        foreach ($charArray as $item){
+            switch ($item){
+                case "char_upper" : $char .= Util::makeCode(self::CHAR_UPPER, 1); break;
+                case "char_lower" : $char .= Util::makeCode(self::CHAR_LOWER, 1); break;
+                case "number" : $char .= Util::makeCode(self::NUMBER, 1); break;
+                case "special" : $char .= Util::makeCode(self::SPECIAL, 1); break;
+            }
+        }
+        return $char;
+    }
 
     public static function parseChar($charArray = []){
         $char = "";
