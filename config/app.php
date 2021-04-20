3c07f164a9f499ca5ba624649347107ed5961a10
wukun <wukun@360.cn>
2021-04-20 00:43:31 -0700

    add youmeng

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'name'      => env('APP_NAME', 'Laravel'),
+    'name'      => env('APP_NAME', 'BOX3'),
     'env'       => env('APP_ENV', 'production'),
     'debug'     => env('APP_DEBUG', false),
     'url'       => env('APP_URL', 'http://localhost'),
